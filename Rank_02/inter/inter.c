626b3023ed88776ce7aa5f6639dd953e17ae8307
mabriand_42 <briandmarie@outlook.fr>
2021-01-20 09:14:36 +0100

    simplifying code

--- Rank_02/inter/inter.c
+++ Rank_02/inter/inter.c
@@ -1,16 +1,5 @@
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-
-int ft_strlen(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
 
 int     ft_isalready(char c, char *str, int pos)
 {
@@ -19,7 +8,7 @@ int     ft_isalready(char c, char *str, int pos)
     i = 0;
     while (i < pos)
     {
-        if (c == str[i])
+        if (str[i] == c)
             return (1);
         i++;
     }
@@ -41,7 +30,7 @@ void    ft_inter(char *s1, char *s2)
             if (s1[i] == s2[j] && ft_isalready(s1[i], s1, i) == 0)
             {
                 write(1, &s1[i], 1);
-                j = 0;
+                //j = 0;
                 break;
             }
             else
@@ -49,20 +38,16 @@ void    ft_inter(char *s1, char *s2)
         }
         i++;
     }
+    write(1, "\n", 1);
     return ;
 }
 
-int main (int argc, char **argv)
+
+int     main (int argc, char **argv)
 {
     if (argc != 3)
-    {
         write(1, "\n", 1);
-        return (0);
-    }
     else
-    {
         ft_inter(argv[1], argv[2]);
-        write(1, "\n", 1);
-    }
     return (0);
 }
